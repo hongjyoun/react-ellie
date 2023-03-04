2541d885087ff25fca866d32ef737f9cd3c5f627
hongjyoun <hongjyoun@gmail.com>
2023-03-04 19:08:46 +0900

    update

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,7 +7,7 @@ import React from 'react';
 export default function Profile() {
     return (
         <div className='profile'>
-            <img className='photo' src='https://images.unsplash.com/photo-1677782465590-415ac196ebc5' />
+            <img className='photo' src='https://images.unsplash.com/photo-1677782465590-415ac196ebc5' alt='profile'/>
             <h1>James</h1>
             <h1>개발자</h1>
         </div>
