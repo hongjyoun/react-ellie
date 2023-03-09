024a1f306b9eca2436f63d3697344c88f3d0c424
hongjyoun <hongjyoun@gmail.com>
2023-03-09 22:06:55 +0900

    reduce, immer

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppXY from './AppXY';
+import AppMentors from './AppMentors-reducer';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppXY />
+    <AppMentors />
   </React.StrictMode>
 );
 
